b4602dab122c3bbe20e05e6bda5b5c005450a26a
Akiitu <akiitu1@gmail.com>
2020-02-26 22:59:30 +0900

    test

--- test/js/index.js
+++ test/js/index.js
@@ -1,7 +1,26 @@
-var server = require('ws').Server;
-var s = new server ({port: 5001});
+//var server = require('ws').Server;
+//var s = new server ({port: 5001});
 
-s.on('connection', function(ws){
+var http = require('http').createServer(handler);
+var html = require('fs').readFileSync('../index.html');
+
+function handler(request, response) {
+	response.writeHead(200,{'Content-Type': 'text/html'});
+	response.end(html);
+}
+
+http.listen(3000);
+
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+
+	socket.on('chat', function(msg){
+		io.emit('chat', msg);
+	});
+});
+
+/*s.on('connection', function(ws){
 
 	ws.on('message', function(message) {
 		s.clients.forEach(function(client){
@@ -13,4 +32,4 @@ s.on('connection', function(ws){
 		console.log('I lost a client');
 	});
 
-});
+});*/
