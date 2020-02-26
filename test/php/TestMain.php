8dcc85526cf4e65f5c8301b1883e9a34c8fdcef5
Akiitu <akiitu1@gmail.com>
2020-02-26 22:02:03 +0900

    test

---
+++ test/php/TestMain.php
@@ -0,0 +1,112 @@
+<?php
+?>
+
+<!doctype html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="../css/Test.css">
+<title>TestMain</title>
+</head>
+<body>
+	<div id="backgroundcolor">
+		<header>
+			<p id="title">スタイルシート実験室</p>
+		</header>
+		<div id="main">
+			<main>
+				<div id="main_outer_div">
+					<table id="hab1_td_hedder">
+						<tr>
+							<th width="120">日付</th>
+							<th width="110">内容</th>
+							<th width="100">名称</th>
+							<th width="70">金額</th>
+						</tr>
+					</table>
+					<div id="main_inner_div">
+						<table id="hab1_tb">
+							<tr>
+								<td width="120">2020-02-17</td>
+								<td width="110">食費</td>
+								<td width="100">昼食代</td>
+								<td width="70">1000</td>
+							</tr>
+							<tr>
+								<td>2020-02-17</td>
+								<td>水道光熱費</td>
+								<td>昼食代</td>
+								<td>1000</td>
+							</tr>
+							<tr>
+								<td>2020-02-17</td>
+								<td>食費</td>
+								<td>昼食代</td>
+								<td>1000</td>
+							</tr>
+							<tr>
+								<td>2020-02-17</td>
+								<td>食費</td>
+								<td>昼食代</td>
+								<td>1000</td>
+							</tr>
+							<tr>
+								<td>2020-02-17</td>
+								<td>食費</td>
+								<td>昼食代</td>
+								<td>1000</td>
+							</tr>
+							<tr>
+								<td>2020-02-17</td>
+								<td>食費</td>
+								<td>昼食代</td>
+								<td>1000</td>
+							</tr>
+							<tr>
+								<td>2020-02-17</td>
+								<td>食費</td>
+								<td>昼食代</td>
+								<td>1000</td>
+							</tr>
+							<tr>
+								<td>2020-02-17</td>
+								<td>食費</td>
+								<td>昼食代</td>
+								<td>1000</td>
+							</tr>
+							<tr>
+								<td>2020-02-17</td>
+								<td>食費</td>
+								<td>昼食代</td>
+								<td>1000</td>
+							</tr>
+							<tr>
+								<td>2020-02-17</td>
+								<td>食費</td>
+								<td>昼食代</td>
+								<td>1000</td>
+							</tr>
+							<tr>
+								<td>2020-02-17</td>
+								<td>食費</td>
+								<td>昼食代</td>
+								<td>1000</td>
+							</tr>
+							<tr>
+								<td>2020-02-17</td>
+								<td>食費</td>
+								<td>昼食代</td>
+								<td>1000</td>
+							</tr>
+							<tr>
+								<td>2020-02-17</td>
+								<td>食費</td>
+								<td>昼食代</td>
+								<td>1000</td>
+							</tr>
+						</table>
+					</div>
+				</div>
+			</main>
+		</div>
+	</div>
+</body>
