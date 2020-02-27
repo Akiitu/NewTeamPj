082296f947bc123b0bdcdc93c2eab0b701d8b1ba
Akiitu <akiitu1@gmail.com>
2020-02-27 14:03:20 +0900

    test

--- test/js/app.js
+++ test/js/app.js
@@ -1,3 +1,29 @@
-/**
- * 
- */
+
+//必要なモジュールを読み込む
+var http = require('http');
+var socketio = require('socket.io');
+var html = require('fs');
+
+//HTTPサーバを生成する
+var server = http.createServer(function(req, res{
+	response.writeHead(200,{'Content-Type': 'text/html'});
+	response.end(fs.readFileSync(process.cwd() + '/index.html', 'utf-8'));
+
+	console.log(__dirname + '/index.html')
+
+}).listen(3000); //ポート競合の場合は値を変更
+
+//HTTPサーバにソケットを紐付ける(WebSocket有効化)
+var io = socketio.listen(server);
+
+//connectionイベント・データを受信する
+io.sockets.on('connection', function(socket) {
+
+	//client_to_serverイベント・データを受信する
+	socket.on('client_to_server', function(data) {
+
+		//server_to_clientイベントデータを送信する
+		io.sockets.emit('server_to_client', {value : data.value});
+	});
+});
+
